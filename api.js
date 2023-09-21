5c1f088077597445e7e20ad8fc31188a66d416e5
Yadavjiii <131160675+Yadavjiii@users.noreply.github.com>
2023-09-21 17:09:33 +0530

    Delete api.js

--- api.js
+++
@@ -1,8 +0,0 @@
-fetch('https://api.github.com/users/hiteshchoudhary')
-.then((response) => {
-    return response.json()
-})
-.then((data) => {
-    console.log(data);
-})
-.catch((error) => console.log(error))
